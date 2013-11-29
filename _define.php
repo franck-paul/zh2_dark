d9fad5c7b63b76307f0f3798853122df284331df
franck <carnet.franck.paul@gmail.com>
2013-11-29 12:51:11 +0100

    First release 1.1 (as zh2 parent)

---
+++ _define.php
@@ -0,0 +1,25 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of zh2, a theme for Dotclear 2.
+# based on WP eponym theme from Leo Babuta (http://leobabauta.com and http://zenhabits.net/theme)
+#
+# Copyright (c) Franck Paul and contributors
+# carnet.franck.paul@gmail.com
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */			"zh2-dark",
+	/* Description*/	"Zen habits (based on eponym Wordpress theme designed by Leo Babuta), dark variation",
+	/* Author */		"Franck Paul",
+	/* Version */		'1.1',
+	/* Parent */		'zh2',
+	array(
+		/* Type */		'type' =>			'theme'
+	)
+);
---
+++ _public.php
@@ -0,0 +1,16 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of zh2, a theme for Dotclear 2.
+# based on WP eponym theme from Leo Babuta (http://leobabauta.com and http://zenhabits.net/themes)
+#
+# Copyright (c) Franck Paul and contributors
+# carnet.franck.paul@gmail.com
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+if (!defined('DC_RC_PATH')) { return; }
+
+l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/main');

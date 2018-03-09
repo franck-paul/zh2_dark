6af3c929361c3e1f50ac904354340673e981910d
franck <carnet.franck.paul@gmail.com>
2018-03-09 13:12:08 +0100

    Various code formatting

--- _define.php
+++ _define.php
@@ -1,25 +1,23 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of zh2, a theme for Dotclear 2.
-# based on WP eponym theme from Leo Babuta (http://leobabauta.com and http://zenhabits.net/theme)
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief zh2 dark, a theme for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Themes
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+if (!defined('DC_RC_PATH')) {return;}
 
 $this->registerModule(
-	/* Name */			"zh2-dark",
-	/* Description*/	"Zen habits (based on eponym Wordpress theme designed by Leo Babuta), dark variation",
-	/* Author */		"Franck Paul",
-	/* Version */		'1.3',
-	/* Parent */		'zh2',
-	array(
-		/* Type */		'type' =>			'theme'
-	)
+    "zh2-dark",                                                                            // Name
+    "Zen habits (based on eponym Wordpress theme designed by Leo Babuta), dark variation", // Description
+    "Franck Paul",                                                                         // Author
+    '1.4',                                                                                 // Version
+    'zh2',                                                                                 // Parent
+    array(
+        'type' => 'theme' // Type
+    )
 );
--- _public.php
+++ _public.php
@@ -1,16 +1,16 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of zh2, a theme for Dotclear 2.
-# based on WP eponym theme from Leo Babuta (http://leobabauta.com and http://zenhabits.net/themes)
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief zh2 dark, a theme for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Themes
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+namespace themes\zh2_dark;
 
-l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/main');
+if (!defined('DC_RC_PATH')) {return;}
+
+\l10n::set(dirname(__FILE__) . '/locales/' . $_lang . '/main');

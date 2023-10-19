1522eb3aeb8c1947c29e75859c8c06763575e9f6
franck <carnet.franck.paul@gmail.com>
2023-10-19 07:23:50 +0200

    WIP: 2.28. compliance

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Franck Paul',
     '4.0',
     [
-        'requires' => [['core', '2.27'], ['php', '8.1']],
+        'requires' => [['core', '2.28']],
         'type'     => 'theme',
         'parent'   => 'zh2',
 
